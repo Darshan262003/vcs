ba437a967e0b74e31abb8bbe01e9ff85ed128220
Darshan262003 <darudarshan2207@gmail.com>
2025-05-28 13:01:16 +0530

    28

--- src/new.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-function Message()
-{
-    return(
-        <h1>This is first class of FS</h1>
-    );
-}
-export default Message;
--- src/newz.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-class Letter extends Component{
-    render()
-    {
-        return(<p>This is a class component</p>);
-    }
-
-}
-export default Letter;
